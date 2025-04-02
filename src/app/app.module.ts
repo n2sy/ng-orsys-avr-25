1c4b1e0e29e16660141e834203d22e0b31776f38
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2025-04-02 11:39:26 +0100

    création du composant ms-word

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { CommonModule } from '@angular/common';
 import { HomeAccountComponent } from './accounts/home-account/home-account.component';
 import { AddAccountComponent } from './accounts/add-account/add-account.component';
 import { ItemAccountComponent } from './accounts/item-account/item-account.component';
+import { MsWordComponent } from './ms-word/ms-word.component';
 
 @NgModule({
   declarations: [
@@ -26,6 +27,7 @@ import { ItemAccountComponent } from './accounts/item-account/item-account.compo
     HomeAccountComponent,
     AddAccountComponent,
     ItemAccountComponent,
+    MsWordComponent,
   ],
   imports: [BrowserModule, FormsModule, CommonModule],
   providers: [],
---
+++ src/app/ms-word/ms-word.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ms-word',
+  standalone: false,
+  templateUrl: './ms-word.component.html',
+  styleUrl: './ms-word.component.css',
+})
+export class MsWordComponent {
+  bgColor = 'yellow';
+  txtColor = 'pink';
+}
