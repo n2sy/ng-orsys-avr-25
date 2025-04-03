3d8bc4503977ca7f8ed1b96f415321a246274a2f
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2025-04-03 14:57:29 +0100

    add children routes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,14 +5,57 @@ import { ManageServersComponent } from './manage-servers/manage-servers.componen
 import { HomeAccountComponent } from './accounts/home-account/home-account.component';
 import { MsWordComponent } from './ms-word/ms-word.component';
 import { InfosComponent } from './infos/infos.component';
+import { AddComponent } from './add/add.component';
+import { EditComponent } from './edit/edit.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
+//2eme version
 let myRoutes: Routes = [
   { path: '', component: AccueilComponent },
-  { path: 'cv', component: CvComponent },
-  { path: 'cv/:id', component: InfosComponent },
+  {
+    path: 'cv',
+    component: CvComponent,
+    children: [
+      //  { path: '', component: CvComponent },
+      { path: 'add', component: AddComponent },
+      {
+        path: ':id',
+        children: [
+          { path: '', component: InfosComponent },
+          { path: 'edit', component: EditComponent },
+        ],
+      },
+    ],
+  },
   { path: 'servers', component: ManageServersComponent },
   { path: 'accounts', component: HomeAccountComponent },
   { path: 'ms-word', component: MsWordComponent },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: '**', redirectTo: 'not-found' }, // Wild Route
 ];
+
+//1ere version
+// let myRoutes: Routes = [
+//   { path: '', component: AccueilComponent },
+//   {
+//     path: 'cv',
+//     children: [
+//       { path: '', component: CvComponent },
+//       { path: 'add', component: AddComponent },
+//       {
+//         path: ':id',
+//         children: [
+//           { path: '', component: InfosComponent },
+//           { path: 'edit', component: EditComponent },
+//         ],
+//       },
+//     ],
+//   },
+//   { path: 'servers', component: ManageServersComponent },
+//   { path: 'accounts', component: HomeAccountComponent },
+//   { path: 'ms-word', component: MsWordComponent },
+//   { path: 'not-found', component: NotFoundComponent },
+//   { path: '**', redirectTo: 'not-found' }, // Wild Route
+// ];
 
 export const INETEUM_ROUTING = RouterModule.forRoot(myRoutes);
